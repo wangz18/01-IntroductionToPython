9d34766d305009508b6e0a3327323673ae361c9f
wangz18 <wangz18@rose-hulman.edu>
2018-03-05 10:48:12 -0500

    ...

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -17,7 +17,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 # TODO: 1.
 #   Yes, that means for YOU to DO things per the following instructions:
 #
-#   On Line 13 above, replace  Zhiyu Wang  with your OWN name.
+#   On Line 13 above, replace PUT_YOUR_NAME_HERE with your OWN name.
 #
 #   BTW, the top block of text above forms what is called a DOC-STRING.
 #   It documents what this module does, in a way that exterior programs
